2a6057ab12a511ead19b0552c9ce968655f5c8c3
Haralampi Staykov <work@haralampi.com>
2023-10-18 11:04:09 +0200

    Add db delete

--- v1/go/db/query.go
+++ v1/go/db/query.go
@@ -226,6 +226,44 @@ func (os ObjectSet[objT, idT, shardKeyT]) SelectByID(id idT, shardKeys ...shardK
 	return
 }
 
+func (os ObjectSet[objT, idT, shardKeyT]) Delete(id idT, shardKeys ...shardKeyT) (err error) {
+
+	table, ok := typeToTable[os.objType]
+	if !ok {
+		err = ErrObjectTypeNotRegistered
+		return
+	}
+
+	if !table.Sharding && len(shardKeys) > 0 {
+		err = ErrObjectNotUsingShards
+		return
+	}
+
+	var dbs []*sql.DB
+	if table.Sharding {
+		dbs = dbsForShardKeys(shardKeys...)
+	} else {
+		dbs = []*sql.DB{Default()}
+	}
+
+	for _, db := range dbs {
+
+		var bytes []byte
+
+		err = db.
+			QueryRow(`DELETE FROM "`+table.RuntimeTableName+`" WHERE id=$1`, id).
+			Scan(&bytes)
+		if err == sql.ErrNoRows {
+			err = nil
+			continue
+		}
+		if err != nil {
+			return
+		}
+	}
+	return
+}
+
 func (os ObjectSet[objT, idT, shardKeyT]) Select(where string, params ...any) (obs []objT, err error) {
 
 	table, ok := typeToTable[os.objType]
