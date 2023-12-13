c3c6dd531dd1a346ae28784812d92f1669276bdc
Haralampi Staykov <work@haralampi.com>
2023-12-13 21:53:42 +0100

    Add convDB.SafeUpdate

--- v1/go/db/query.go
+++ v1/go/db/query.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"encoding/json"
 	"errors"
+	"fmt"
 	"reflect"
 )
 
@@ -261,5 +262,103 @@ func (os ObjectSet[objT, idT, shardKeyT]) Delete(id idT, shardKeys ...shardKeyT)
 			return
 		}
 	}
+	return
+}
+
+func (os ObjectSet[objT, idT, shardKeyT]) SafeUpdate(from, to objT) (err error) {
+
+	table, ok := typeToTable[os.objType]
+	if !ok {
+		err = ErrObjectTypeNotRegistered
+		return
+	}
+
+	fromTrail, toTrail := from.Trail(), to.Trail()
+
+	if fromTrail.ID != toTrail.ID {
+		err = errors.New("cannot safely update object with different IDs")
+		return
+	}
+
+	if fromTrail.ShardKey != toTrail.ShardKey {
+		err = errors.New("cannot safely update object with different shard keys")
+		return
+	}
+
+	var db *sql.DB
+	if table.Sharding {
+		db = dbByShardKey(string(fromTrail.ShardKey))
+	} else {
+		db = Default()
+	}
+
+	var (
+		cmp     objT
+		cmpHash string
+	)
+	row := db.QueryRow(`SELECT "object", md5("object") FROM "`+table.RuntimeTableName+`" WHERE "id"=$1`, fromTrail.ID)
+	err = row.Scan(&cmp, &cmpHash)
+	if err == sql.ErrNoRows {
+		return fmt.Errorf("object with ID '%s' does not exist", fromTrail.ID)
+	}
+	if err != nil {
+		return
+	}
+
+	cmpBytes, err := json.Marshal(cmp)
+	if err != nil {
+		return
+	}
+
+	fromBytes, err := json.Marshal(from)
+	if err != nil {
+		return
+	}
+
+	if string(cmpBytes) != string(fromBytes) {
+		return fmt.Errorf("object with ID '%s' has been modified since it was retrieved", fromTrail.ID)
+	}
+
+	tx, err := db.Begin()
+	if err != nil {
+		return
+	}
+	defer func() {
+		if err != nil {
+			err = errors.Join(
+				err,
+				tx.Rollback(),
+			)
+			return
+		}
+		err = tx.Commit()
+	}()
+
+	toBytes, err := json.Marshal(to)
+	if err != nil {
+		return
+	}
+
+	res, err := tx.Exec(`UPDATE "`+table.RuntimeTableName+`" SET "object"=$1, "updated_at"=$2, "updated_by"=$3 WHERE "id"=$4 AND md5("object")=$5`,
+		toBytes, toTrail.UpdatedAt, toTrail.UpdatedBy, toTrail.ID, cmpHash)
+	if err != nil {
+		return
+	}
+
+	count, err := res.RowsAffected()
+	if err != nil {
+		return
+	}
+
+	if count == 0 {
+		return fmt.Errorf("object with ID '%s' has been modified since it was retrieved", fromTrail.ID)
+	}
+
+	_, err = tx.Exec(`INSERT INTO "`+table.HistoryTableName+`" SELECT * FROM "`+table.RuntimeTableName+`" WHERE "id"=$1`,
+		toTrail.ID)
+	if err != nil {
+		return
+	}
+
 	return
 }
